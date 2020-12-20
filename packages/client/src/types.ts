0054e01ecaacaf86728a4c97957e80696e57f77b
HerbCaudill <herb@devresults.com>
2020-12-20 13:04:48 +0100

    client/types.ts:

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -7,3 +7,33 @@ export interface PeerOptions {
   id: string
   url: string
 }
+
+export namespace Message {
+  export type ClientToServer = Join
+
+  export interface Join {
+    type: 'Join' | 'Leave'
+    join?: string[] // document IDs
+    leave?: string[]
+  }
+
+  export type ServerToClient = Introduction
+
+  export interface Introduction {
+    type: 'Introduction'
+    id: string // the other peer we're introducing this client to
+    keys: string[] // document IDs
+  }
+}
+
+export declare enum ConnectionEvent {
+  OPEN = 'open',
+  READY = 'ready',
+  CONNECTION = 'connection',
+  CLOSE = 'close',
+  ERROR = 'error',
+  PEER = 'peer',
+  PEER_REMOVE = 'peer_remove',
+  MESSAGE = 'message',
+  DATA = 'data',
+}
