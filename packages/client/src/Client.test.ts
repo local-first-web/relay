bcbee46657bfa903bcaf312bc713b883f940e9aa
HerbCaudill <herb@devresults.com>
2021-01-20 11:42:25 +0100

    client/Client.test.ts: refactor

--- packages/client/src/Client.test.ts
+++ packages/client/src/Client.test.ts
@@ -1,19 +1,14 @@
 import { Server } from '@localfirst/relay'
-import debug from 'debug'
 import { getPortPromise as getAvailablePort } from 'portfinder'
 import { Client, PeerEventPayload } from './Client'
 import { PEER } from './constants'
 
 describe('client', () => {
-  const log = debug('lf:relay-client:tests')
   let port: number
   let url: string
 
   let server: Server
-  let key: string
   let testId: number = 0
-  let aliceId: string
-  let bobId: string
 
   beforeAll(async () => {
     // find a port and set things up
@@ -24,67 +19,97 @@ describe('client', () => {
     server.listen({ silent: true })
   })
 
-  beforeEach(() => {
-    testId += 1
-    aliceId = `alice-${testId}`
-    bobId = `bob-${testId}`
-    key = `test-key-${testId}`
-    log(`TEST ${testId}`)
-  })
-
   afterAll(() => {
     server.close()
   })
 
   describe('connections', () => {
     const setup = () => {
-      const alice = new Client({ id: aliceId, url })
-      const bob = new Client({ id: bobId, url })
+      testId += 1
+      const key = `test-key-${testId}`
+
+      const alice = new Client({ id: `alice-${testId}`, url })
+      const bob = new Client({ id: `bob-${testId}`, url })
+      const charlie = new Client({ id: `charlie-${testId}`, url })
 
       alice.join(key)
       bob.join(key)
-      return { alice, bob }
+      charlie.join(key)
+
+      return { alice, bob, charlie, key }
     }
 
-    describe('join', () => {
-      it('should connect to a peer', async () => {
+    describe('Alice and Bob both join', () => {
+      beforeEach(() => {})
+      it('joins a key and connects to a peer', async () => {
         const { alice, bob } = setup()
+        await connected(alice, bob)
 
-        await Promise.all([
-          new Promise<void>((resolve) => {
-            alice.on(PEER, ({ id }: PeerEventPayload) => {
-              expect(id).toEqual(bobId)
-              resolve()
-            })
-          }),
-          new Promise<void>((resolve) => {
-            bob.on(PEER, ({ id }: PeerEventPayload) => {
-              expect(id).toEqual(aliceId)
-              resolve()
-            })
-          }),
-        ])
+        expect(alice.peers.has(bob.id)).toBe(true)
+        expect(bob.peers.has(alice.id)).toBe(true)
       })
     })
 
     describe('leave', () => {
-      it('should ', () => {})
+      it('leaves a key', async () => {
+        const { alice, bob, key } = setup()
+        await connected(alice, bob)
+
+        expect(alice.peers.has(bob.id)).toBe(true)
+        expect(alice.getSocket(bob.id, key)).not.toBeUndefined()
+        expect(alice.keys).toContain(key)
+
+        // Alice decides she's no longer interested in this topic
+        alice.leave(key)
+
+        expect(alice.peers.has(bob.id)).toBe(true) // still have an entry for Bob
+        expect(alice.getSocket(bob.id, key)).toBeUndefined() // but not for this key
+        expect(alice.keys).not.toContain(key)
+      })
     })
 
-    describe('disconnect from one', () => {
-      it('should ', () => {})
+    describe('disconnect from one peer', () => {
+      it('disconnects', async () => {
+        const { alice, bob } = setup()
+        await connected(alice, bob)
+
+        alice.disconnect(bob.id)
+
+        // both are disconnected
+        expect(alice.peers.has(bob.id)).toBe(false)
+        expect(bob.peers.has(alice.id)).toBe(false)
+      })
     })
 
-    describe('disconnect from all', () => {
-      it('should ', () => {})
+    describe('disconnect from all peers', () => {
+      it('disconnects', async () => {
+        const { alice, bob, charlie } = setup()
+        await Promise.all([connected(alice, bob), connected(alice, charlie)])
+
+        alice.disconnect()
+
+        expect(alice.peers.has(bob.id)).toBe(false)
+        expect(alice.peers.has(charlie.id)).toBe(false)
+        expect(bob.peers.has(alice.id)).toBe(false)
+        expect(charlie.peers.has(alice.id)).toBe(false)
+      })
     })
 
     describe('disconnect then reconnect', () => {
-      it('should ', () => {})
+      it('should ', async () => {
+        const { alice, bob, key } = setup()
+        await connected(alice, bob)
+
+        // Alice disconnects
+        alice.disconnect()
+
+        // Alice reconnects
+        alice.join(key)
+      })
     })
 
     describe('send/receive', () => {
-      it('should send a message to a remote peer', (done) => {
+      it('should send a message to a remote peer', async (done) => {
         const { alice, bob } = setup()
 
         alice.on(PEER, ({ socket }: PeerEventPayload) => {
@@ -101,3 +126,12 @@ describe('client', () => {
     })
   })
 })
+
+const connected = (a: Client, b: Client) => Promise.all([connection(a, b), connection(b, a)])
+
+const connection = (a: Client, b: Client) =>
+  new Promise<void>((resolve) =>
+    a.on(PEER, ({ id }) => {
+      if (id === b.id) resolve()
+    })
+  )
