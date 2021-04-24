27a672910fbcf61891ecc75e29a76fa231f70a1a
HerbCaudill <herb@devresults.com>
2021-04-24 15:10:51 +0200

    client/isReady.ts: use WebSocket.OPEN constant

--- packages/client/src/isReady.ts
+++ packages/client/src/isReady.ts
@@ -2,6 +2,6 @@
 
 export const isReady = async (socket: WebSocket) =>
   new Promise<void>(async (resolve, reject) => {
-    while (socket.readyState !== 1) await pause(100)
+    while (socket.readyState !== WebSocket.OPEN) await pause(100)
     resolve()
   })
