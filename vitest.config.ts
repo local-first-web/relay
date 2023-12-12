3320290610b6f36f87902ae64f0b62c65650ec15
Herb Caudill <herb@devresults.com>
2023-12-12 16:13:32 +0100

    vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov", "text", "html"],
+      skipFull: true,
+      exclude: [
+        "**/fuzz",
+        "**/helpers",
+        "**/coverage",
+        "examples/**/*",
+        "docs/**/*",
+        "**/test/**/*",
+      ],
+    },
+  },
+})
